8bba4bf37c6aed9bc3940140fc17e5356647197f
janmay <janiceparungao@gmail.com>
2018-09-27 00:08:01 -0700

    thru 5a

--- sakila-homework.sql
+++ sakila-homework.sql
@@ -29,22 +29,39 @@ FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
 -- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, so create a column in the table actor named description and use the data type BLOB (Make sure to research the type BLOB, as the difference between it and VARCHAR are significant).
-
+ALTER TABLE actor
+ADD COLUMN description BLOB AFTER last_name;
 
 -- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the description column.
+
 -- 4a. List the last names of actors, as well as how many actors have that last name.
+SELECT last_name, COUNT(*) AS 'Number of Actors' 
+FROM actor GROUP BY last_name;
+
 -- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+SELECT last_name, COUNT(*) AS 'Number of Actors' 
+FROM actor GROUP BY last_name HAVING count(*) >=2;
+
 -- 4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
+UPDATE actor 
+SET first_name = 'HARPO'
+WHERE first_name = "Groucho" AND last_name = "Williams";
+
 -- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
+UPDATE actor 
+SET first_name = 'GROUCHO'
+WHERE first_name = "HARPO" AND last_name = "Williams";
+
 
 -- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
-
+DESCRIBE sakila.address;
 
 -- Hint: https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html
 
-
-
 -- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
+SELECT first_name, last_name, address
+
+
 -- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
 -- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
 -- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
